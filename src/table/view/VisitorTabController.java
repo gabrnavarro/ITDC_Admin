679fe38ac116127fa5813a0d6a0625008ea60fc2
jdlives <jlaborada.97@gmail.com>
2016-10-27 02:24:47 +0800

    Integrate niyo na GUI niyo

--- src/table/view/VisitorTabController.java
+++ src/table/view/VisitorTabController.java
@@ -45,7 +45,7 @@ public class VisitorTabController implements Initializable{
 		try{
 			Connection conn = dc.Connect();
 			data = FXCollections.observableArrayList();
-			ResultSet rs = conn.createStatement().executeQuery("SELECT firstname,lastname,middlename,purpose,organization,office FROM students");
+			ResultSet rs = conn.createStatement().executeQuery("SELECT firstname,lastname,middlename, purpose,organization,office FROM visitors");
 			while(rs.next()){
 				
 				data.add(new Visitor(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6)));
